148d245405d0091ef45328a3a9e0e5563544500c
Fernando Medina <fernando@linux.site>
2015-03-12 10:49:45 -0430

    continuando categorias

--- protected/views/categorias/index.php
+++ protected/views/categorias/index.php
@@ -3,9 +3,18 @@
 /* @var $dataProvider CActiveDataProvider */
 ?>
 <h1>Categorias</h1>
+<div id="lista_menu">
+	<ul>
+		<li>
+			<?php echo CHtml::button('Crear Categoria', array('class'=>'boton_peque', 'submit'=>array('productos/create'))) ; ?>
+		</li>
+	</ul>
+</div>
+<div id="caja_lista">
 
 <?php $this->widget('zii.widgets.CListView', array(
 	'summaryText'=>'',
 	'dataProvider'=>$dataProvider,
 	'itemView'=>'/administrador/categorias',
 )); ?>
+</div>
